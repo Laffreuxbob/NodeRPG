4d8ff55b9483850f22f44ed365a03e75b786abec
Unknown <olivier.bricaud@sfr.fr>
2019-01-16 23:51:08 +0100

    rename Soldier to Warrior class

--- src/js/Soldier.js
+++
@@ -1,9 +0,0 @@
-class Soldier {
-    constructor(breed, name, hp, strength, itemPoints) {
-      this.breed = breed;
-      this.name = name;
-      this.hp = hp;
-      this.strength = strength;
-      this.itemPoints = 10
-    }
-  }
