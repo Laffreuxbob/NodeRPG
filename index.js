86cd1698543516799975eaf5698abfe2d8e880a6
Laffreuxbob <olivier.bricaud@sfr.fr>
2019-01-16 16:49:23 +0100

    node server, first request (version pkg.json)

---
+++ index.js
@@ -0,0 +1,43 @@
+// Server
+const express = require('express');
+const conf = require('./utils/config.js')
+const pkg = require('./package.json'); // Pour pouvoir lire les data du package.json
+
+const server = express();
+
+
+// BDD
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'rpg'
+});
+
+// Facon presque propre d'eviter le probleme de header CORS
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+server.use(allowCrossDomain);
+
+
+// Methode GET pour recuperer la version du projet
+// curl http://127.0.0.1:8080/version
+server.get('/version', (req, res) => {
+    if (!pkg || !pkg.version) {
+        console.log('Error: No package.json');
+        res.status(404);
+        return res.send();
+    }
+    res.status(200);
+    console.log('Version: ' + pkg.version);
+    res.send(JSON.stringify(pkg.version));  
+})
+
+server.listen(conf.port, conf.hostname, function() {   
+    console.log('Server running at http://' + conf.hostname + ':' + conf.port + '/');
+  });
